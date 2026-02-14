78e21b76990b1c047257b7b948d7b33091f8a6e6
arm <arm.dev024@gmail.com>
2026-02-14 12:23:36 +0545

    Add CORS middleware to FastAPI application for improved cross-origin resource sharing

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+cors = CORSMiddleware(
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.add_middleware(cors)
 
 
 @app.get("/")
