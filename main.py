d2ddfb3133553ee2cb3d436e1369d56ebb970906
arm <arm.dev024@gmail.com>
2026-02-14 12:29:18 +0545

    Refactor CORS middleware integration in main.py by directly adding it to the FastAPI app instance.

--- main.py
+++ main.py
@@ -3,14 +3,13 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-cors = CORSMiddleware(
+app.add_middleware(
+    CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-app.add_middleware(cors)
 
 
 @app.get("/")
